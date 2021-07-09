3ed5ce8917d5bde2b82e499ad0c9631933aafc81
yumattchoco <86557607+yumattchoco@users.noreply.github.com>
2021-07-09 18:54:44 +0900

    a

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
 
 	$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
 
-	//$signature = $_SERVER['HTTP_'.\LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+	$signature = $_SERVER['HTTP_'.\LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 
-$events = $bot->parseEventRequest(/*file_get_contents('php://input'), $signature*/);
+	$events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
 
 	foreach ($events as $event) {
 		replyTextMessage($bot, $event->getReplyToken(), 'TextMessage');
