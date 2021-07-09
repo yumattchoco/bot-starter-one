bcc12ba0465a4bb7807f1f0d643a1f15bfb69a97
yumattchoco <86557607+yumattchoco@users.noreply.github.com>
2021-07-09 17:06:12 +0900

    com

--- index.php
+++ index.php
@@ -1,7 +1,27 @@
 <?php
 	require_once __DIR__ . '/vendor/autoload.php';
 
-	$inputString = file_get_contents('php://input');
-	error_log($inputString);
+	$httpClient = new Line\Linebot\HTTPClient\curlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+
+	$bot = new LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+
+	$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+
+	$events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+
+	foreach ($events as $event) {
+		replyTextMessage($bot, $event->getReplyToken(), 'TextMessage');
+	}
+
+
+
+	function replyTextMessage($bot, $replyToken, $text) {
+
+		$response = $bot->replyMessage($replyToken, new \LINE\LINEbot\MessageBuilder\TextMessageBilder($text));
+
+		if (!$response->is_succeeded()) {
+			error_log('Failed!'.$response->getHTTPStatus.' '.$response->getRawBody());
+		}
+	}
 
 ?>
