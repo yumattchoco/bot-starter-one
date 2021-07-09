438793d684648ad25057421cc1f4e9582a0c39db
yumattchoco <86557607+yumattchoco@users.noreply.github.com>
2021-07-09 18:12:17 +0900

    a

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 		$response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text));
 
 		if (!$response->isSucceeded()) {
-			error_log('Failed!'.$response->getHTTPStatus.' '.$response->getRawBody());
+			error_log('Failed!'.$response->getHTTPStatus().' '.$response->getRawBody());
 		}
 	}
 
