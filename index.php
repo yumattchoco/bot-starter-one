bb9b58f7e78a70a2583799c6b2bf1d515e06bc07
yumattchoco <86557607+yumattchoco@users.noreply.github.com>
2021-07-09 22:11:52 +0900

    m

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 		$response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text));
 
 		if (!$response->isSucceeded()) {
-			error_log('Failed!'.$response->getHTTPStatus().' '.$response->getRawBody());
+			error_log('Failed! '.$response->getHTTPStatus().' '.$response->getRawBody());
 		}
 	}
 
