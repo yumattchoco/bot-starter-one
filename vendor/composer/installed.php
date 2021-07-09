ce14f3b82435003bcc92da3f6f35273dab5b3783
yumattchoco <86557607+yumattchoco@users.noreply.github.com>
2021-07-09 16:09:09 +0900

    com

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '590d70d014c9109bdaba67a55e0ac45523188663',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '590d70d014c9109bdaba67a55e0ac45523188663',
+            'dev_requirement' => false,
+        ),
+        'linecorp/line-bot-sdk' => array(
+            'pretty_version' => '7.0.0',
+            'version' => '7.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../linecorp/line-bot-sdk',
+            'aliases' => array(),
+            'reference' => '210d87af3f5fa3ed7837c3c875e39e1e3caeb9b7',
+            'dev_requirement' => false,
+        ),
+    ),
+);
